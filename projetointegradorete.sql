04e7f86790a7848bd9b4a6cd68af85947391e6d4
HarysRafael <69483666+HarysRafael@users.noreply.github.com>
2021-07-12 14:06:14 -0300

    Update projetointegradorete.sql

--- projetointegradorete.sql
+++ projetointegradorete.sql
@@ -5,4 +5,18 @@ email_funcionario varchar(60),
 telefone_funcionario varchar (20),
 dataContrato_funcionario date
 );
-
+CREATE TABLE `tbl_cardapio` (
+  `id_cardapio` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `nome_cardapio` varchar(20),
+  `valor_cardapio` double ,
+);
+CREATE TABLE `tbl_cliente` (
+  `id_cliente` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `nome_cliente` varchar(30),
+  `email_cliente` varchar(60),
+  `telefone_cliente` varchar(20),
+  `rua_cliente` varchar(20),
+  `bairro_cliente` varchar(20),
+  `cidade_cliente` varchar(20),
+  `datacadastro_cliente` varchar(20),
+  ) 
