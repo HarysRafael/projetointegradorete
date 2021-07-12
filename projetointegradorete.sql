396d0ce63f4c9253d5f69ad90b067ac0fcbec911
HarysRafael <harys.rafael@tds.company>
2021-07-12 13:47:09 -0300

    Telas principal, cliente, funcionaro e banco de dados

--- projetointegradorete.sql
+++ projetointegradorete.sql
@@ -1,2 +1,8 @@
+create table tbl_funcionario(
+id_funcionario int auto_increment primary key,
+nome_funcionario varchar(60),
+email_funcionario varchar(60),
+telefone_funcionario varchar (20),
+dataContrato_funcionario date
+);
 
-select * from tbl_Cliente;
